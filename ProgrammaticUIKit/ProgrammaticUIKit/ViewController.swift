31d621cec052528af819a920af4f7ba1a926dedd
Yunus Oktay <ynsokty.3@gmail.com>
2025-01-28 15:20:28 +0300

    init

---
+++ ProgrammaticUIKit/ProgrammaticUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticUIKit
+//
+//  Created by Yunus Oktay on 28.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
